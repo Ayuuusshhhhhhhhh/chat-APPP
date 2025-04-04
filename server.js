a1c1d9cae194e372acddf87dc0b17634fa534bd9
Ayush Baranwal <156538186+Ayuuusshhhhhhhhh@users.noreply.github.com>
2025-04-04 16:13:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,53 +1,62 @@
 const path = require("path");
-const express = require("express");
 const http = require("http");
+const express = require("express");
 const socketio = require("socket.io");
+
+const formatMessage = require("./messages");
+const { userJoin, getCurrentUser, userLeaves, getRoomUsers } = require("./users");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-const formatMessage = require("./messages"); // No 'utils' folder
-const { userJoin, getCurrentUser, userLeaves, getRoomUsers } = require("./users");
-
 const botName = "FreeChat Bot";
 
-// Serve static files (CSS, JS, etc.) from the current folder
-app.use(express.static(__dirname));
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
-// Serve `index.html` when accessing `/`
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// Socket.io events
 io.on("connection", (socket) => {
+    console.log("New connection...");
+
+    // User joins a room
     socket.on("joinRoom", ({ username, room }) => {
         const user = userJoin(socket.id, username, room);
         socket.join(user.room);
 
+        // Welcome message
         socket.emit("message", formatMessage(botName, "Welcome to FreeChat!"));
 
+        // Notify others
         socket.broadcast.to(user.room).emit(
             "message",
             formatMessage(botName, `${user.username} has joined the chat!`)
         );
 
+        // Update room users
         io.to(user.room).emit("roomUsers", {
             room: user.room,
             users: getRoomUsers(user.room),
         });
     });
 
+    // Listen for messages
     socket.on("chatMessage", (msg) => {
         const user = getCurrentUser(socket.id);
-        io.to(user.room).emit("message", formatMessage(user.username, msg));
+        if (user) {
+            io.to(user.room).emit("message", formatMessage(user.username, msg));
+        }
     });
 
+    // Handle disconnect
     socket.on("disconnect", () => {
         const user = userLeaves(socket.id);
         if (user) {
-            io.to(user.room).emit("message", formatMessage(botName, `${user.username} has left the chat!`));
+            io.to(user.room).emit(
+                "message",
+                formatMessage(botName, `${user.username} has left the chat!`)
+            );
+
+            // Update users in room
             io.to(user.room).emit("roomUsers", {
                 room: user.room,
                 users: getRoomUsers(user.room),
@@ -56,5 +65,6 @@ io.on("connection", (socket) => {
     });
 });
 
+// Use port from environment or default to 3000
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
