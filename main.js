aaad06cfc0a671bcddc57f0e8955aa9145a16f41
Ayush Baranwal <156538186+Ayuuusshhhhhhhhh@users.noreply.github.com>
2025-04-04 16:15:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,50 +1,38 @@
 const socket = io();
 
-const chatMessages = document.querySelector(".chat-messages");
-const chatForm = document.getElementById("chat-form");
-const roomName = document.getElementById("room-name");
-const userList = document.getElementById("users");
-
+// Get username and room from URL
 const { username, room } = Qs.parse(location.search, {
-  ignoreQueryPrefix: true,
+    ignoreQueryPrefix: true,
 });
 
+// Join chatroom
 socket.emit("joinRoom", { username, room });
 
-socket.on("roomUsers", ({ room, users }) => {
-  outputRoomName(room);
-  outputUsers(users);
-});
-
+// Listen for messages from server
 socket.on("message", (message) => {
-  outputMessage(message);
-  chatMessages.scrollTop = chatMessages.scrollHeight;
+    console.log("New message:", message);
+    displayMessage(message);
 });
 
-chatForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const message = e.target.elements.msg.value;
-  socket.emit("chatMessage", message);
-  e.target.elements.msg.value = "";
-  e.target.elements.msg.focus();
+// Update users in the room
+socket.on("roomUsers", ({ room, users }) => {
+    document.getElementById("room-name").innerText = room;
+    document.getElementById("users").innerHTML = users.map(user => `<li>${user.username}</li>`).join("");
 });
 
-function outputMessage(message) {
-  const div = document.createElement("div");
-  div.classList.add("message");
-  div.innerHTML = `<p class="meta">${message.username}<span>  ${message.time}</span></p>
-                    <p class="text">
-                        ${message.text}
-                    </p>`;
-  document.querySelector(".chat-messages").appendChild(div);
-}
+// Send message
+document.getElementById("chat-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const msg = e.target.elements.msg.value;
+    socket.emit("chatMessage", msg);
+    e.target.elements.msg.value = "";
+});
 
-function outputRoomName(room) {
-  roomName.innerText = room;
-}
-
-function outputUsers(users) {
-  userList.innerHTML = `${users
-    .map((user) => `<li>${user.username}</li>`)
-    .join("")}`;
+// Function to display message
+function displayMessage(message) {
+    const div = document.createElement("div");
+    div.classList.add("message");
+    div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+                     <p class="text">${message.text}</p>`;
+    document.querySelector(".chat-messages").appendChild(div);
 }
